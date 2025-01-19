f66b0b63b3d7d928b888efc9fb86b1d37b03a519
Udi Fogiel <udifoglle@gmail.com>
2025-01-19 17:33:51 +0200

    update build script

--- build.lua
+++ build.lua
@@ -13,14 +13,30 @@ packtdszip = true
 typesetexe = "optex"
 typesetfiles = {"lualineno.opm"}
 ctanzip = module
-checkconfigs = {"configfiles/config-optex", "configfiles/config-latex"}
-specialformats  = {optex = {luatex = {binary = "optex", format = ""}}}
+
+checkconfigs = {"configfiles/config-optex", "configfiles/config-latex", "configfiles/config-plain"}
+specialformats.optex  = {luatex = {binary = "optex", format = ""}}
+specialformats.plain  = {luatex = {binary = "luatex", format = ""}}
+
+tdslocations =
+  {
+    "tex/optex/lualineno/*.opm",
+    "tex/lualatex/lualineno/*.sty",
+    "tex/luatex/lualineno/*.tex",
+    "tex/luatex/lualineno/*.lua",
+  }
+
+specialtypesetting = specialtypesetting or {}
+function optex_doc()
+    run('.', "optex -jobname lualineno-doc '\\docgen lualineno'")
+end
+specialtypesetting["lualineno.opm"] = {func = optex_doc}
 
 tagfiles = sourcefiles
 function update_tag(file,content,tagname,tagdate)
   if string.match(file, "%.opm$") then
     return string.gsub(content,
-      "lualineno_version {%d+.%d+.%d+, %d%d%d%d-%d%d-%d%d",
+      "lualineno_version {%d+.%d+, %d%d%d%d-%d%d-%d%d",
       "lualineno_version {" .. tagname .. ", " .. tagdate)
   elseif string.match(file, "%.lua$") then
     return content
@@ -36,8 +52,7 @@ end
 function pre_release()
     call({"."}, "tag")
     call({"."}, "ctan", {config = options['config']})
-    rm("*", "*.pdf")
-    rm("*", "*.tds.zip")
+    rm(".", "*.tds.zip")
 end
 
 target_list["prerelease"] = { func = pre_release, 
